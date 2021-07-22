8aa037e40d9251c3032be684c4110afaaff3c20d
lillee205 <leelillian205@gmail.com>
2021-07-22 13:52:20 -0700

    final version with fixed flashcards

--- src/flashcards.js
+++ src/flashcards.js
@@ -1,65 +1,30 @@
 export const flashcards = [
   {
     title: "korean",
-    numCards: "14",
+    numCards: "4",
     dateCreated: "1/27/2019",
     dateModified: "3/29/2020",
     desc:
-      "korean " +
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore sed omnis soluta incidunt? Dolorum, a laboriosam. Possimus facere veniam iste voluptate aliquam blanditiis officiis porro. Sequi cupiditate rem pariatur asperiores! Sunt, minus. Magni vel consectetur et laborum, quisquam veritatis ratione? Dolores saepe nobis voluptas, delectus aut quisquam! Consequuntur, magnam officiis.",
-    cards: [
-      ["hello", "안녕하세요"],
-      ["im hungry", "배고파"],
+      "This is a Korean study set made for students to practice random words. ㅂ를 무섭게 보시줘",
+      cards: [
+      ["안녕하세요", "hello"],
+      ["취미", "hobby"],
+      ["재수없다", "[someone is] full of themself"],
+      ["박귀벌래", "cockroach"],
     ],
   },
   {
     title: "japanese",
-    numCards: "17",
+    numCards: "3",
     dateCreated: "1/24/2019",
     dateModified: "3/22/2020",
     desc:
-      "japanese " +
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore sed omnis soluta incidunt? Dolorum, a laboriosam. Possimus facere veniam iste voluptate aliquam blanditiis officiis porro. Sequi cupiditate rem pariatur asperiores! Sunt, minus. Magni vel consectetur et laborum, quisquam veritatis ratione? Dolores saepe nobis voluptas, delectus aut quisquam! Consequuntur, magnam officiis.",
+      "This is a Japanese flashcard set made specifically for practicing kanji.",
 
     cards: [
-      ["hello", "안녕하세요"],
-      ["im hungry", "배고파"],
-    ],
-  },
-  {
-    title: "greek",
-    numCards: "24",
-    dateCreated: "1/27/2013",
-    dateModified: "3/29/2014",
-    desc:
-      "greek " +
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore sed omnis soluta incidunt? Dolorum, a laboriosam. Possimus facere veniam iste voluptate aliquam blanditiis officiis porro. Sequi cupiditate rem pariatur asperiores! Sunt, minus. Magni vel consectetur et laborum, quisquam veritatis ratione? Dolores saepe nobis voluptas, delectus aut quisquam! Consequuntur, magnam officiis.",
-
-    cards: [
-      ["hello", "안녕하세요"],
-      ["im hungry", "배고파"],
-    ],
-  },
-  {
-    title: "german",
-    numCards: "34",
-    dateCreated: "2/27/2002",
-    dateModified: "3/29/2014",
-    desc:
-      "german " +
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore sed omnis soluta incidunt? Dolorum, a laboriosam. Possimus facere veniam iste voluptate aliquam blanditiis officiis porro. Sequi cupiditate rem pariatur asperiores! Sunt, minus. Magni vel consectetur et laborum, quisquam veritatis ratione? Dolores saepe nobis voluptas, delectus aut quisquam! Consequuntur, magnam officiis.",
-
-    cards: [
-      ["hello", "안녕하세요"],
-      ["im hungry", "배고파"],
-      ["hello", "안녕하세요"],
-      ["im hungry", "배고파"],
-      ["hello", "안녕하세요"],
-      ["im hungry", "배고파"],
-      ["hello", "안녕하세요"],
-      ["im hungry", "배고파"],
-      ["hello", "안녕하세요"],
-      ["im hungry", "배고파"]
+      ["悪い (わるい)", "bad"],
+      ["安い(やすい)", "cheap"],
+      ["暗い(くらい)"], "dark"],
     ],
   },
 ];
